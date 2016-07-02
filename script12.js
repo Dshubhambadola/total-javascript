bda15361e30c861d832b8a582304bf0227ee7d38
Shubham Badola <shubham.badola@medibox.in>
2016-07-02 10:58:01 +0530

    fourth commit

---
+++ script12.js
@@ -0,0 +1,60 @@
+/**
+ * Created by shubham on 22/5/16.
+ */
+/*var superman = {
+  status:"ready",
+    rescueBatman : function(){
+    document.write('locating batman ass');
+}
+}
+if(superman.status === "ready"){
+    superman.rescueBatman();
+}
+var utility = {
+    printAllmember : function(shubham){
+      for (i in shubham){
+          document.write("<br />" + shubham[i]);
+      }
+    }
+}
+utility.printAllmember(superman);*/
+var football = {
+    team : 'england,brazil,germany',
+    match : 'one',
+    otherTeams: {
+        teams :function(){
+            document.write('south africa, kenya');
+        }
+
+    },
+
+maneger: [
+    {
+        name : 'vidit'
+    },
+    {
+        name : 'arif'
+    },
+    {
+       name : 'sagar'
+    }
+]
+};
+
+/*if(typeof football.shoes === 'undefined'){
+    football.shoes = 'nike';
+}
+document.write(football.shoes);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
